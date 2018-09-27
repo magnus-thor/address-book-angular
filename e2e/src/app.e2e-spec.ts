252eb70fffaa4540e36fb3d085020d032cef99ef
Magnus <magnus.thor.ar@gmail.com>
2018-09-27 11:39:20 +0200

    WIP finding last card created

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,8 +7,28 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display create contact header', () => {
     page.navigateTo();
     expect(page.getParagraphText()).toEqual('Create contact');
   });
+
+  it('fill in the form', () => {
+    page.navigateTo();
+    page.getField('name').sendKeys('John Doe');
+    page.getField('email').sendKeys('john@craftacademy.se');
+    page.getField('company').sendKeys('Craft Academy');
+    page.getField('role').sendKeys('Tester');
+    page.getField('twitter').sendKeys('@tester');
+    page.getField('location').sendKeys('Gothenburg');
+    page.getField('notes').sendKeys('Some tester guy');
+    page.getSubmitButton().click();
+    // .then(function() {
+    //   const card = page.lastCard();
+    //   // console.log('HELLO HELLO', card);
+    //   expect(page.elementByTag(card, 'h1')).toEqual('John Doe');
+    // })
+    page.lC()
+
+  });
+  
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -6,6 +6,37 @@ export class AppPage {
   }
 
   getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+    return element(by.css('app-root h2')).getText();
+  }
+
+  getField(name) {
+    return element(by.name(name));
+  }
+
+  getSubmitButton() {
+    return element(by.buttonText('Submit'));
+  }
+
+  lastCard() {
+    browser.sleep(10000000)
+    let cards = element.all(by.css('.card'));
+    // console.log('SWEET SWEET', cards);
+    let lastCard = cards[cards.length -1];
+    return lastCard;
+  }
+
+  lC() {
+    browser.sleep(100000);
+    let cards = element.all(by.css('.card')).count();
+
+    return cards;
+  }
+
+  elementByTag(card, tag) {
+    return card.element(by.css(tag));
+  }
+
+  sleep(time) {
+    browser.sleep(time);
   }
 }
